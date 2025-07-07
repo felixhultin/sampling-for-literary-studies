b55cd4c00fe8ff796e1d592a6f8019126ef11036
Felix <felixhultin@yahoo.se>
2025-07-07 11:30:44 +0200

    scripts to sample from kubhist, phase 1

--- _sample_kubhist2.sh
+++ _sample_kubhist2.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+target_words=($(csvcut mtp_target_words.tsv -t -c "words,PoS" | tr "," "_" | tail -n +2))
+echo "Uses following target words: ${target_words[@]}"
 # Sampling for t1, phase 1
 kubhist_1880=($(ls -d data/corpora/kubhist2/*1880*.xml.bz2))
 echo "Starting to process the following files: ${kubhist_1880[@]}"
 python sample_kubhist2.py \
-    -w fru_NN fruga_NN husmoder_NN \
+    -w ${target_words[@]} \
     -c ${kubhist_1880[@]} \
     -s "1880-01-01" \
-    -e "1883-12-31"
+    -e "1883-12-31" \
+    -o "t1_kubhist"
 
 # Sampling for t9, phase 1
 kubhist_1910=($(ls -d data/corpora/kubhist2/*1910*.xml.bz2))
@@ -15,7 +18,7 @@ kubhist_1920=($(ls -d data/corpora/kubhist2/*1920*.xml.bz2))
 kubhist_1910_1920=("${kubhist_1910[@]}" "${kubhist_1920[@]}")
 echo "Starting to process the following files: ${kubhist_1910_1920[@]}"
 python sample_kubhist2.py \
-    -w fru_NN fruga_NN husmoder_NN \
+    -w ${target_words[@]} \
     -c ${kubhist_1910_1920[@]} \
     -s "1918-01-01" \
     -e "1922-12-31"
--- sample_kubhist2.py
+++ sample_kubhist2.py
@@ -15,27 +15,11 @@ logging.basicConfig(level='INFO')
 SBX_METADATA_API_ENDPOINT = "https://ws.spraakbanken.gu.se/ws/metadata/v3/"
 
 
-def open_file(file_path, mode='r', encoding='utf-8'):
+def open_file(file_path, mode='rb'):
     if file_path.endswith('.bz2'):
-        return bz2.open(file_path, mode=mode)#, encoding=encoding)
+        return bz2.open(file_path, mode=mode)
     else:
-        return open(file_path, mode=mode)#, encoding=encoding)
-
-
-def count_tags(xml_file, tag: str = 'text'):
-    count = 0
-    for _, elem in ET.iterparse(xml_file, events=('end',)):
-        if elem.tag == tag:
-            count += 1
-            elem.clear()
-    # try:
-    #     for _, elem in ET.iterparse(xml_file, events=('end',)):
-    #         if elem.tag == 'sentence':
-    #             count += 1
-    #             elem.clear()
-    # except xml.etree.ElementTree.ParseError:
-    #     return
-    return count
+        return open(file_path, mode=mode)
 
 def _fetch_metadata(api_endpoint : str, resource_name: str):
     metadata_query = f"{api_endpoint}?resource={resource_name}"
@@ -69,41 +53,51 @@ def extract_target_usages(xml_file, targets : set[tuple], start_date: str, end_d
     resource_name = Path(Path(xml_file).stem).stem
     total_sentences = get_nof_sentences_from_resource(resource_name)
     logging.info(f"Start processing: {xml_file}")
-    with open_file(xml_file) as f_in, jsonlines.open(f'{resource_name}_target_usages.jsonl', mode='w') as writer, tqdm(total=total_sentences, desc="Processing") as pbar:
-        context = ET.iterparse(f_in, events=('start', 'end'))  
+    if args.output_folder:
+        output_folder = args.output_folder
+    else:
+        output_folder = 'output'
+    Path(output_folder).mkdir(parents=True, exist_ok=True)
+    with open_file(xml_file) as f_in, tqdm(total=total_sentences, desc="Processing") as pbar:
+        target_json_writer = {
+            (t,pos): jsonlines.open(f'{output_folder}/{resource_name}_{t}_{pos}_target_usages.jsonl', mode='w') for t, pos in targets
+        }
+        context = ET.iterparse(f_in, events=('start', 'end'))
         for event, elem in context:
             if event == 'start':
                 if elem.tag == 'text':
                     text_date = elem.get('date')
                     process_current_text = timeperiod_in_range(start_date, end_date, text_date)
                 if elem.tag == 'sentence' and process_current_text:
-                    offsets, matches = [], []
+                    offsets, matches, pos_tags = [], [], []
                     sentence = ""
                 if elem.tag == 'token' and process_current_text:
                     text = elem.text if elem.text is not None else "<PARSE_ERROR>"
                     lemmas_string = elem.get('lemma')
                     pos = elem.get('pos')
                     lemmas = [l for l in lemmas_string.split('|')]
-                    for lemma in lemmas: 
+                    for lemma in lemmas:
                         if (lemma, pos) in targets:
                             start = len(sentence)
                             end = len(sentence) + len(text) - 1
                             offsets.append( (start, end) )
                             matches.append(lemma)
+                            pos_tags.append(pos)
                     sentence += text + " "
                         
             elif event == 'end':
                 if elem.tag == 'sentence':
                     if process_current_text:
-                        # Remove trailing space
-                        sentence = sentence[:-1]
-                        for off, match in zip(offsets, matches):
+                        sentence = sentence[:-1] # Remove trailing space
+                        for off, match, pos_tag in zip(offsets, matches, pos_tags):
                             start, end = off
+                            writer = target_json_writer[match, pos_tag]
                             writer.write({
                                 'id': elem.get('id'),
                                 'start': start,
                                 'end': end,
                                 'target': match,
+                                'pos_tag': pos_tag,
                                 'sentence': sentence,
                                 'date': text_date
                             })
@@ -122,7 +116,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                     prog='Samples words from corpora',
                     description='Samples sentences given words and corpora')
-    parser.add_argument('-w', '--word', nargs='+')
+    parser.add_argument('-w', '--word', nargs='+') # W/o nargs='+' because of spaces.
     parser.add_argument('-c', '--corpora', nargs='+')
     parser.add_argument('-o', '--output-folder', default="")
     parser.add_argument('-s', '--start')
@@ -132,9 +126,16 @@ if __name__ == '__main__':
     corpora = args.corpora
     output_folder = args.output_folder
     targets = []
-    for w in args.word:
+    for idx, w in enumerate(args.word):
         try:
-            w, pos = w.split("_")
+            splitted = w.split("_")
+            # For edge case with verb and preposition, for example: 'backa upp'
+            if len(splitted) == 3:
+                verb, preposition, pos = splitted
+                w = f"{verb} {preposition}"
+                args.word[idx] = f"{w}_{pos}"
+            else:
+                w, pos = splitted
         except:
             raise ValueError("-w or --word must of format <word>_<part-of-speech>")
 
