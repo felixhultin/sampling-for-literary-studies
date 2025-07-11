709a74fec32d9534f40eefef241a956c29e1373e
Felix <felixhultin@yahoo.se>
2025-07-11 12:03:14 +0200

    finished with random sampling

--- post_sampling.py
+++ post_sampling.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import pandas as pd
+import shutil
 
 from pathlib import Path
 
@@ -32,7 +33,7 @@ def read_output_dir_into_df(output_dir: str):
 
     df_list = []
     for file_path in jsonl_files:
-        df = pd.read_json(file_path, lines=True)
+        df = pd.read_json(file_path, lines=True, dtype= {"date": str})
         
         basename = os.path.basename(file_path)
         resource_name = basename.split('_')[0]
@@ -44,10 +45,13 @@ def read_output_dir_into_df(output_dir: str):
     
     return pd.concat(df_list, ignore_index=True)
 
-def filter_json_files_on_length(jsonl_files, min_len : int = 20, max_len : int = 50, output_dir = 'filtered_jsonl_files'):
-    Path(output_dir).mkdir(parents=True, exist_ok=False)
+def filter_json_files_on_length(jsonl_files, min_len : int = 20, max_len : int = 100, output_dir = 'filtered_jsonl_files'):
+    out_dir = Path(output_dir)
+    if out_dir.exists() and out_dir.is_dir():
+        shutil.rmtree(out_dir)
+    out_dir.mkdir(parents=True, exist_ok=False)
     for file_path in jsonl_files:
-        df = pd.read_json(file_path, lines=True)
+        df = pd.read_json(file_path, lines=True, dtype= {"date": str})
         if len(df) < 1:
             continue
         df_filter = df[
@@ -56,75 +60,93 @@ def filter_json_files_on_length(jsonl_files, min_len : int = 20, max_len : int =
         ]
         basename = os.path.basename(file_path)
         print(f"Filtered {file_path} down from {len(df)} to {len(df_filter)}")
-        df_filter.to_json(f"{output_dir}/{basename}", orient='records', lines=True)
+        df_filter.to_json(f"{output_dir}/{basename}", orient='records', lines=True, force_ascii=False)
+
+def combine_json_files_on_word(jsonl_files, output_dir = 'combined'):
+    output_files = {}
+    for file_path in jsonl_files:
+        basename = os.path.basename(file_path)
+        splitted = basename.split('_')
+        rest = "_".join(splitted[1:])
+        value = output_files.setdefault(rest, [])
+        value.append(file_path)
+    out_dir = Path(output_dir)
+    if out_dir.exists() and out_dir.is_dir():
+        shutil.rmtree(out_dir)
+    out_dir.mkdir(parents=True, exist_ok=False) 
+    for w, file_paths in output_files.items():
+        to_combine = []
+        for fp in file_paths:
+            basename = os.path.basename(fp)
+            resource = basename.split('_')[0]
+            df = pd.read_json(fp, lines=True, dtype= {"date": str})
+            df['resource'] = resource
+            to_combine.append(df)
+        combined = pd.concat(to_combine)
+        combined.to_json(f"{output_dir}/{w}", orient='records', lines=True, force_ascii=False)
 
 
-def filter_sentences(
-        df: pd.DataFrame, min_len : int = 20, max_len : int = 50):
-    df_filter = df[
-        (df['sentence'].str.split().str.len() > min_len) & \
-        (df['sentence'].str.split().str.len() <= max_len)
-    ]
-    df_target_pos_count = df_filter\
-        .groupby(['target', 'pos_tag'])\
-        .size()\
-        .reset_index(name='counts')
-    df_target_pos_count_gt_sample_size = \
-        df_target_pos_count[df_target_pos_count['counts'] >= sample_size]
-    df_filter = df_filter.set_index(['target', 'pos_tag'])
-    raise NotImplementedError
+def random_sample_json_files(jsonl_files, output_dir = 'random_sample', min_sample_size = 20, max_extra_samples = 20):
+    out_dir = Path(output_dir)
+    if out_dir.exists() and out_dir.is_dir():
+        shutil.rmtree(out_dir)
+    out_dir.mkdir(parents=True, exist_ok=False)
+    for file_path in jsonl_files:
+        basename = os.path.basename(file_path)
+        df = pd.read_json(file_path, lines=True, dtype= {"date": str})
+        if len(df) < min_sample_size:
+            continue
+        else:
+            if len(df) > min_sample_size + max_extra_samples:
+                random_sample = df.sample(min_sample_size + max_extra_samples)
+            else:
+                random_sample = df.sample(len(df))
+        random_sample.to_json(f"{output_dir}/{basename}", orient='records', lines=True, force_ascii=False)
+
 
 if __name__ == '__main__':
-    t1_svt = glob.glob(os.path.join('t1_svt_twoAI', '*.jsonl'))
-    t9_svt = glob.glob(os.path.join('t9_svt_twoAI', '*.jsonl'))
-    df = read_frequencies_from_files(t1_svt + t9_svt)
-    df['decade'] = df['decade']\
-        .replace({'1880': 't1_kubhist', 
-                  '1910': 't9_kubhist', 
-                  '1920': 't9_kubhist', 
-                  '2006': 't1_svt', 
-                  '2007': 't1_svt', 
-                  '2022': 't9_svt',
-                  '2023': 't9_svt'
-                })
-    df = df[['decade', 'target', 'count']].groupby(['decade', 'target']).sum('count').reset_index()
-    df = df.pivot(index='target', columns='decade', values='count')
-    df = df[['t1_svt', 't9_svt']]
-    mtp_target_words = pd.read_csv('mtp_target_words.tsv', sep='\t')
-    right_order = list(mtp_target_words.words + '_' + mtp_target_words.PoS)
-    # Fix "backa upp"
-    right_order = ["backa upp_VB" if w == "backa_upp_VB" else w for w in right_order]
-    df = df.loc[right_order]
+    # Post sample for SVT
+    t1_svt = glob.glob(os.path.join('t1_svt', '*.jsonl'))
+    t9_svt = glob.glob(os.path.join('t9_svt', '*.jsonl'))
 
-    # CONTINUE HERE
-    t1_svt_filtered = 'svt_t1_filtered'
-    df = filter_json_files_on_length(t1_svt, min_len=15, max_len=100, output_dir = t1_svt_filtered )
-    t1_svt_filtered_files = glob.glob(os.path.join(t1_svt_filtered, '*.jsonl'))
-    df = read_frequencies_from_files(t1_svt_filtered_files)
-    df = df[['decade', 'target', 'count']].groupby(['decade', 'target']).sum('count').reset_index()
-    df = df.pivot(index='target', columns='decade', values='count')
-    df = df[['t1_svt', 't9_svt']]
-    mtp_target_words = pd.read_csv('mtp_target_words.tsv', sep='\t')
-    right_order = list(mtp_target_words.words + '_' + mtp_target_words.PoS)
-    # Fix "backa upp"
-    right_order = ["backa upp_VB" if w == "backa_upp_VB" else w for w in right_order]
-    df = df.loc[right_order]
-
-    #df = read_output_dir_into_df('t1_kubhfile_pathist')
-    #df_filter = select_valid_sentences(df)
-    # # Get sentences with no less than 20 or more than 50 tokens
-    # df_filter = df[
-    #     (df['sentence'].str.split().str.len() > 20) & \
-    #     (df['sentence'].str.split().str.len() <= 50)
-    # ]
-    # sample_size = 30
-    # df_target_pos_count = df_filter\
-    #     .groupby(['target', 'pos_tag'])\
-    #     .size()\
-    #     .reset_index(name='counts')
-    # df_target_pos_count_gt_sample_size = \
-    #     df_target_pos_count[df_target_pos_count['counts'] >= sample_size]
-    # df_filter = df_filter.set_index(['target', 'pos_tag'])
-  
+    t1_svt_filtered_output_dir = 't1_svt_filtered'
+    t9_svt_filtered_output_dir = 't9_svt_filtered'
+    filter_json_files_on_length(t1_svt, min_len=15, max_len=100, output_dir = t1_svt_filtered_output_dir )
+    filter_json_files_on_length(t9_svt, min_len=15, max_len=100, output_dir = t9_svt_filtered_output_dir )
     
-    #df_random_sample = df_filter.groupby(['target', 'pos_tag']).sample(30)
+    t1_svt_filtered_files = glob.glob(os.path.join(t1_svt_filtered_output_dir, '*.jsonl'))
+    t9_svt_filtered_files = glob.glob(os.path.join(t9_svt_filtered_output_dir, '*.jsonl'))
+    t1_svt_combined_output_dir = 't1_svt_combined'
+    t9_svt_combined_output_dir = 't9_svt_combined'
+    combine_json_files_on_word(t1_svt_filtered_files, t1_svt_combined_output_dir)
+    combine_json_files_on_word(t9_svt_filtered_files, t9_svt_combined_output_dir)
+
+    t1_svt_combined_files = glob.glob(os.path.join(t1_svt_combined_output_dir, '*.jsonl'))
+    t9_svt_combined_files = glob.glob(os.path.join(t9_svt_combined_output_dir, '*.jsonl'))
+    t1_svt_random_sample_output_dir = 't1_svt_random_samples'
+    t9_svt_random_sample_output_dir = 't9_svt_random_samples'
+    random_sample_json_files(t1_svt_combined_files, output_dir=t1_svt_random_sample_output_dir)
+    random_sample_json_files(t9_svt_combined_files, output_dir=t9_svt_random_sample_output_dir)
+
+    # Post sample for kubhist
+    t1_kubhist = glob.glob(os.path.join('t1_kubhist_new', '*.jsonl'))
+    t9_kubhist = glob.glob(os.path.join('t9_kubhist_new', '*.jsonl'))
+
+    t1_kubhist_filtered_output_dir = 't1_kubhist_filtered'
+    t9_kubhist_filtered_output_dir = 't9_kubhist_filtered'
+    filter_json_files_on_length(t1_kubhist, min_len=15, max_len=100, output_dir = t1_kubhist_filtered_output_dir )
+    filter_json_files_on_length(t9_kubhist, min_len=15, max_len=100, output_dir = t9_kubhist_filtered_output_dir )
+    
+    t1_kubhist_filtered_files = glob.glob(os.path.join(t1_kubhist_filtered_output_dir, '*.jsonl'))
+    t9_kubhist_filtered_files = glob.glob(os.path.join(t9_kubhist_filtered_output_dir, '*.jsonl'))
+    t1_kubhist_combined_output_dir = 't1_kubhist_combined'
+    t9_kubhist_combined_output_dir = 't9_kubhist_combined'
+    combine_json_files_on_word(t1_kubhist_filtered_files, t1_kubhist_combined_output_dir)
+    combine_json_files_on_word(t9_kubhist_filtered_files, t9_kubhist_combined_output_dir)
+
+    t1_kubhist_combined_files = glob.glob(os.path.join(t1_kubhist_combined_output_dir, '*.jsonl'))
+    t9_kubhist_combined_files = glob.glob(os.path.join(t9_kubhist_combined_output_dir, '*.jsonl'))
+    t1_kubhist_random_sample_output_dir = 't1_kubhist_random_samples'
+    t9_kubhist_random_sample_output_dir = 't9_kubhist_random_samples'
+    random_sample_json_files(t1_kubhist_combined_files, output_dir=t1_kubhist_random_sample_output_dir)
+    random_sample_json_files(t9_kubhist_combined_files, output_dir=t9_kubhist_random_sample_output_dir)
